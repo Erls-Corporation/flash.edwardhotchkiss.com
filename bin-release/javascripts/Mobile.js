0026ab46cd1906ac521621beec22f9361c3bb0c8
Edward Hotchkiss <e@ingk.com>
2011-10-26 18:53:05 -0400

    can't delete it, can't keep it on my computer -

--- bin-release/javascripts/Mobile.js
+++
@@ -1,9 +0,0 @@
-
-function isiPhone() { 
-    var agent = navigator.userAgent.toLowerCase(); 
-    return agent.match(/iPhone/i);
-};
-
-if (isiPhone()) {
-    window.location = 'http://www.edwardhotchkiss.mobi/iPhone/';
-};
